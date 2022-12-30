8ab81b23ce6a49d699ceaec8f628a853033c48d4
afrobug <afrojahmed6@gmail.com>
2022-12-30 20:09:26 +0530

    for of

--- functionality.js
+++ functionality.js
@@ -1,3 +1,26 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+// }
+
+// let arr = [1, 6, 33, "afroj", 44, "ahmed"]
+// for (let x of arr) {
+//     console.log(x);
+// }                                                                           //for of
+
+
+// const person = { fname: "John", lname: "Doe", age: 25 };                       // for in
+// for (let x in person) {
+//     console.log(person[x]);
+// }
+
+// var arr1 = [1, 2, 4, 5, 6];
+// var arr2 = [2, 3, 5, 7];
+
+// // var result = [];
+// for (let x of arr1) {
+//     if (arr2.includes(x)) {
+//         // result.push(x);
+//         console.log(x)
+//     }
+// }
+// console.log(result);
